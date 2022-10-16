c7aaebb33cc92faa554e035f449b1fc5a993b8ef
peaceoyeleke <oyelekept@gmail.com>
2022-10-16 16:04:31 -0700

    header file

--- main.h
+++ main.h
@@ -1,8 +1,15 @@
-#ifndef PRINTF
-#define PRINTF
+#ifndef PRINT_F
+#define PRINT_F
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_char(char c);
+int _print_string(char *str);
+int _print_percent(char a);
 
 #endif
