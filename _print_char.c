e851f5133c591e9ea114d0ca4f8d179443cc243e
peaceoyeleke <oyelekept@gmail.com>
2022-10-16 16:31:32 -0700

    printing a single character

---
+++ _print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_char - prints a character
+ * @c: character to be printed
+ * Return: character
+ */
+
+int _print_char(char c)
+{
+	_putchar('c');
+	return (0);
+}
