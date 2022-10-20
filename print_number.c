a1ecbb9fa635beacfc5c8a3e4ba6f85ef13383ce
peaceoyeleke <oyelekept@gmail.com>
2022-10-20 10:32:55 -0700

    printf function

--- print_number.c
+++ print_number.c
@@ -49,20 +49,19 @@ int print_unsgined_number(unsigned int n)
 	int div;
 	int len;
 	unsigned int num;
-	
+
 	div = 1;
 	len = 0;
 	num = n;
-	
+
 	for (; num / div > 9; )
-		div *= 10
-			
-			for (; div != 0; )
+		div *= 10;
+	
+	for (; div != 0; )
 			{
-			len += _write_char('0' + num / div);
+			len += _putchar('0' + num / div);
 			num %= div;
 			div /= 10;
 			}
-			
-			return (len);
+	return (len);
 }
